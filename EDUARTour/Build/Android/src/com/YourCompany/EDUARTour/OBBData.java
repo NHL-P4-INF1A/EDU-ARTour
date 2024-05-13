bc4a6117ea2bcb3538e658be0bb341a8ed857499
Dave <dave.van.den.berg@student.nhlstenden.com>
2024-05-13 11:50:23 +0200

    Added colors to the battlebot and seperated the wheels

--- EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
+++ EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-30661815L // the length of the file in bytes
+30713957L // the length of the file in bytes
 )
 };
 };
