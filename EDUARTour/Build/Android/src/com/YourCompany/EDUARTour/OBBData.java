199be4bdfdfcec5254adf717a72e3d1e43a6907e
Yunus Karakoç <yunus.karakoc@student.nhlstenden.com>
2024-05-27 09:51:23 +0200

    Pop-up met uitleg toegevoegd per project

--- EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
+++ EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-50487670L // the length of the file in bytes
+50487830L // the length of the file in bytes
 )
 };
 };
