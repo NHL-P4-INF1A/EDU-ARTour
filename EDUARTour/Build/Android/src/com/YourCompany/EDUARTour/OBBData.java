23c0d85faf29328ed43f0e07aa18193c6d70f0e2
GideonDijkhuis <146840611+GideonDijkhuis@users.noreply.github.com>
2024-05-31 11:50:27 +0200

    Added first language save system

--- EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
+++ EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-96132536L // the length of the file in bytes
+96136669L // the length of the file in bytes
 )
 };
 };
