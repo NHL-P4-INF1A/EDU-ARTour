f6da01d75dac94efa91328f353ffadc014f1c01d
GideonDijkhuis <146840611+GideonDijkhuis@users.noreply.github.com>
2024-06-04 11:07:34 +0200

    Merge Dev to Startprocedure

--- EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
+++ EDUARTour/Build/Android/src/com/YourCompany/EDUARTour/OBBData.java
@@ -19,11 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-<<<<<<< Updated upstream
-33690205L // the length of the file in bytes
-=======
-49678881L // the length of the file in bytes
->>>>>>> Stashed changes
+96139628L // the length of the file in bytes
 )
 };
 };
